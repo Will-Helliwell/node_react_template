e46d4c7fd366a1d992072f594f7d983d1239cd98
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2022-12-28 11:50:47 +0000

    Setup prettier/eslint, all passing

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json('Welcome!');
+  res.json('Welcome!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`); // eslint-disable-line no-console
 });
